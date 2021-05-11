6a929bde803bdafed693100b727f3a1c7ad4e783
CJO-Smoothstack <Christopher.ONeill@smoothstack.com>
2021-05-11 03:51:32 -0400

    Assignment 1
    Finished

---
+++ src/com/ss/jb/one/Assignment1.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.ss.jb.one;
+
+/** Printing Patterns
+ * @author Christopher O'Neill
+ *
+ */
+public class Assignment1 {
+
+	/** Since the patterns are deterministic
+	 * (No inputs or anything that make them variable length)
+	 * I've opted to hard code the pattern printing for efficiency
+	 * purposes.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//Pattern 1
+		System.out.println("1)");
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println(".........");
+		
+		//Pattern 2
+		System.out.println("2)");
+		System.out.println(".........");
+		System.out.println("****");
+		System.out.println("***");
+		System.out.println("**");
+		System.out.println("*");
+
+		//Pattern 3
+		System.out.println("3)");
+		System.out.println("     *");
+		System.out.println("    ***");
+		System.out.println("   *****");
+		System.out.println("  *******");
+		System.out.println("...........");
+		
+		//Pattern 4
+		System.out.println("4)");
+		System.out.println("...........");
+		System.out.println("  *******");
+		System.out.println("   *****");
+		System.out.println("    ***");
+		System.out.println("     *");
+		
+	}
+
+}
